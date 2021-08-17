cbc628cfd52c62918087f7b512458b5ebc008602
tasim-blh <mandrakechan@gmail.com>
2021-08-17 09:14:08 +0700

    add first route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+const userData = [
+    {name: 'coro'},
+    {name: 'jangkrik'}
+]
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: 'it is success',
+        users: userData
+    })
+})
 app.listen(8000, () => {
     console.log('listening to port 8000')
 })
