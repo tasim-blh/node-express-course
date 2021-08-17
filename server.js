4c34ce5c5d36f2b29f0d86ed7f743eaaac61be06
tasim-blh <mandrakechan@gmail.com>
2021-08-17 15:22:07 +0700

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
 
 const userData = [
     {name: 'coro'},
@@ -21,6 +24,26 @@ app.get('/users/:id', (req, res) => {
         message: 'it is success',
         user: req.params.id
     })
+})
+app.post('/login', (req, res) => {
+    const username = req.body.name
+    const password = req.body.password
+
+    const user = 'joko'
+    const pass = '123456'
+
+    if (username === user && password === pass) {
+        res.json({
+            success: true,
+            message: 'success again',
+            token: 'token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'wrong user or password'
+        })
+    }
 })
 app.listen(8000, () => {
     console.log('listening to port 8000')
