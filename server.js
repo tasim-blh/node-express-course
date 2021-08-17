5e1e99d2b8d1f0f2847f778a1e37a57a6be07c7f
tasim-blh <mandrakechan@gmail.com>
2021-08-17 15:07:02 +0700

    add dynamic route

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/users', (req, res) => {
         users: userData
     })
 })
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'it is success',
+        user: req.params.id
+    })
+})
 app.listen(8000, () => {
     console.log('listening to port 8000')
 })
